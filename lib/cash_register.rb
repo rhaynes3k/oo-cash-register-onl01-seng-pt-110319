016191901ae020488a30b13360849f8ac90f998e
Rommel Haynes <rhaynes3k@gmail.com>
2019-12-23 01:53:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,21 @@
 require 'pry'
 class CashRegister
+<<<<<<< HEAD
   attr_accessor :discount, :total, :qty, :items, :costs, :sub_tot
+=======
+  attr_accessor :discount, :total, :qty, :items
+>>>>>>> 8964ebb49b2f4147b50a20640e6a5a0cc6ad888e
   
   
   def initialize(discount = 0)
     @discount = discount
     @total = 0
     @items = []
+<<<<<<< HEAD
     @costs = []
     @sub_tot = 0
+=======
+>>>>>>> 8964ebb49b2f4147b50a20640e6a5a0cc6ad888e
   end
   
   def total
@@ -16,9 +23,17 @@ class CashRegister
   end
   
   def add_item(i, amt, qty=1)
+<<<<<<< HEAD
     @sub_tot = amt*qty
     @total = self.total + @sub_tot
     qty.times {self.items.push(i)}
+=======
+   
+    @total = self.total + amt*qty
+    qty.times {self.items.push(i)}
+    #last_item = self
+    #binding.pry
+>>>>>>> 8964ebb49b2f4147b50a20640e6a5a0cc6ad888e
   end
   
   def apply_discount
@@ -34,8 +49,12 @@ class CashRegister
     @items
   end
   
+<<<<<<< HEAD
   def void_last_transaction
     @total -= self.sub_tot
   end
   
+=======
+  def self.void
+>>>>>>> 8964ebb49b2f4147b50a20640e6a5a0cc6ad888e
 end
